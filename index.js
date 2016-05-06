d3efcb460de2e6a87a11c0c2b85e3b12487673a7
FDiskas <projektas@gmail.com>
2016-05-06 22:14:07 +0300

    v0.1.0

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 var gulp = require('gulp'),
-	elixir = require('laravel-elixir'),
-	Task = elixir.Task,
-	iconFont = require('gulp-iconfont'),
-	iconFontCss = require('gulp-iconfont-css'),
-	runTimestamp = Math.round(Date.now()/1000),
-	fontName = 'icons';
+    elixir = require('laravel-elixir'),
+    Task = elixir.Task,
+    iconFont = require('gulp-iconfont'),
+    iconFontCss = require('gulp-iconfont-css'),
+    runTimestamp = Math.round(Date.now()/1000),
+    extend = require("xtend");
 
 
-elixir.extend('fonts', function(src, output) {
+elixir.extend('fonts', function(src, output, options) {
 
-	new Task('fonts', function() {
-		return gulp.src(src)
-			.pipe(iconFontCss({
-				fontName: fontName,
-				// path: elixir.config.assetsPath + '/' + elixir.config.css.sass.folder + '/_' + fontName + '.scss',
-				targetPath: '../css/' + fontName + '.css',
-				fontPath: '../fonts/'
-			}))
-			.pipe(iconFont({
-				normalize: true,
-				fontName: fontName, // required
-				prependUnicode: false, // recommended option
-				formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'], // default, 'woff2' and 'svg' are available
-				timestamp: runTimestamp // recommended to get consistent builds when watching files
-			}))
-			.pipe(gulp.dest(output))
-			.pipe(new elixir.Notification('Fonts generated'));
-	}).watch(src);
+    options = options || {};
+    var fontName = '_font_icons',
+    elixirConfig = this.config,
+    config = {
+        font: {
+            normalize: true,
+            fontName: fontName, // required
+            prependUnicode: false, // recommended option
+            formats: ['ttf', 'eot', 'woff', 'woff2', 'svg'], // default, 'woff2' and 'svg' are available
+            timestamp: runTimestamp // recommended to get consistent builds when watching files
+        },
+        css: {
+            fontName: fontName,
+            targetPath: '../../' + elixirConfig.assetsPath + '/' + elixirConfig.css.sass.folder + '/' + fontName + '.scss',
+            fontPath: '../fonts/'
+        }
+    };
+
+    config = extend(config, options);
+    console.log(config);
+
+    new Task('fonts', function() {
+        return gulp.src(src)
+            .pipe(iconFontCss(config.css))
+            .pipe(iconFont(config.font))
+            .pipe(gulp.dest(output));
+    }).watch(src);
 
 });
